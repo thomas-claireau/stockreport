a8016c889174da04905eb2543c3c4f35e386920d
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-10 17:11:35 +0100

    front : add header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,21 @@
+import avatar from '../assets/img/avatar.jpg';
 import style from './Header.module.scss';
 
 export default function Header() {
-	return <div className={style['header']}>Header</div>;
+	return (
+		<header className={style['header']}>
+			<div className="container">
+				<a href="#">
+					<h1>📈 StockReport</h1>
+				</a>
+				<div className={style['personna']}>
+					<img src={avatar} alt="" />
+					<div>
+						<h2>Thomas Claireau</h2>
+						<span>Founder</span>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
