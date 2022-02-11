ae7280985eeedb8aa72c0c192d47657207f8e1c0
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-11 14:00:22 +0100

    add fontawesome

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,14 @@
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header/Header';
 import Summary from './components/Summary/Summary';
+
+config.autoAddCss = false;
+library.add(fas, far, fab);
 
 ReactDOM.render(
 	<React.StrictMode>
