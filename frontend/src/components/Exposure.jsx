8ce6374c33f3cf1fe4bd098f22c33c50db287ab3
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-18 08:18:33 +0100

    init geographic and sector exposure

--- frontend/src/components/Exposure.jsx
+++ frontend/src/components/Exposure.jsx
@@ -1,7 +1,10 @@
-export default function Exposure({ className }) {
+import GeographicExposure from './GeographicExposure';
+import SectorExposure from './SectorExposure';
+
+export default function Exposure({ className, type }) {
 	return (
-		<div
-			className={`${className} h-96 dark:bg-slate-800 p-4 sm:h-auto`}
-		></div>
+		<div className={`${className} h-96 dark:bg-slate-800 p-4 sm:h-auto`}>
+			{type === 'geographic' ? <GeographicExposure /> : <SectorExposure />}
+		</div>
 	);
 }
