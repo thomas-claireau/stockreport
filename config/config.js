b13f32da441b5398a7a6a0ece8d2d8cac4ffe9a9
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-18 17:07:57 +0100

    fix config sequelize

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ const config = {};
 const { NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 
 config[NODE_ENV] = {
-	username: DB_USERNAME,
-	password: DB_PASSWORD,
-	database: process.env[NODE_ENV == 'test' ? 'DB_TEST_NAME' : 'DB_NAME'],
-	host: DB_HOST,
+	username: DB_USERNAME || 'root',
+	password: DB_PASSWORD || '',
+	database: process.env[NODE_ENV == 'test' ? 'DB_TEST_NAME' : 'DB_NAME'] || 'stockreport_test',
+	host: DB_HOST || '127.0.0.1',
 	dialect: 'mysql',
 	dialectOptions: {
 		timezone: 'Europe/Paris',
