baea6ac4f8dfdc2b81523045c12bff392ce136b6
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-20 17:22:24 +0100

    config logging false

--- config/config.js
+++ config/config.js
@@ -6,13 +6,16 @@ const { NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 config[NODE_ENV] = {
 	username: DB_USERNAME || 'root',
 	password: DB_PASSWORD || '',
-	database: process.env[NODE_ENV == 'test' ? 'DB_TEST_NAME' : 'DB_NAME'] || 'stockreport_test',
+	database:
+		process.env[NODE_ENV == 'test' ? 'DB_TEST_NAME' : 'DB_NAME'] ||
+		'stockreport_test',
 	host: DB_HOST || '127.0.0.1',
 	dialect: 'mysql',
 	dialectOptions: {
 		timezone: 'Europe/Paris',
 	},
 	timezone: 'Europe/Paris',
+	logging: NODE_ENV !== 'test',
 };
 
 module.exports = config;
