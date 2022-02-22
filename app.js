6d966c2efd41ee8cf62a06cb869be9544cfd3e18
Thomas Claireau <thomas.claireau@gmail.com>
2022-02-22 14:03:27 +0100

    add config eslint and prettier

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ require('dotenv').config();
 
 // some imports
 const express = require('express');
-const models = require('./models');
-const cmd = require('node-cmd');
-
-// environment variables
-const ENV = process.env.NODE_ENV || 'development';
 
 // Security Imports
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const hpp = require('hpp');
+const models = require('./models');
+
+// environment variables
+const ENV = process.env.NODE_ENV || 'development';
 
 // Express app launching
 const app = express();
@@ -31,23 +30,23 @@ app.use(cors());
 
 // express-rate-limit middleware to limit the amount of request done
 const limiter = rateLimit({
-	windowMs: 30 * 60 * 1000,
-	max: 100,
+  windowMs: 30 * 60 * 1000,
+  max: 100,
 });
 app.use(limiter);
 
 // Setting CORS headers
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Content-Type, Access-Control-Allow-Headers'
-	);
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, DELETE, PATCH, OPTIONS'
-	);
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, Content-Type, Access-Control-Allow-Headers',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  );
+  next();
 });
 
 // Parsing req
@@ -58,11 +57,11 @@ app.use(hpp()); // HPP middleware to protect against HTTP parameter pollution at
 
 // sync models
 if (ENV == 'development') {
-	models.sequelize
-		.sync({ force: true })
-		.then(() => new Seed(models.sequelize.getQueryInterface()).run());
+  models.sequelize
+    .sync({ force: true })
+    .then(() => new Seed(models.sequelize.getQueryInterface()).run());
 } else if (ENV != 'test') {
-	models.sequelize.sync();
+  models.sequelize.sync();
 }
 
 // Setting routes
